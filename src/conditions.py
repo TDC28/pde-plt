484672bf87d2db527bcc123da09f7c67fa88eb9c
TDC28 <a06x03g07t01@protonmail.com>
2024-11-08 21:29:36 -0500

    Creating boundary conditions and IC class

---
+++ src/conditions.py
@@ -0,0 +1,18 @@
+class BoundaryCondition:
+    pass
+
+
+class DirichletBC(BoundaryCondition):
+    pass
+
+
+class NeumannBC(BoundaryCondition):
+    pass
+
+
+class RobinBC(BoundaryCondition):
+    pass
+
+
+class InitialCondition:
+    pass
