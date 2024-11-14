a1ed59cf03bf747a5531a31d2355484d70c6a5c5
TDC28 <a06x03g07t01@protonmail.com>
2024-11-14 14:59:11 -0500

    Removal of CLI class

--- src/main.py
+++ src/main.py
@@ -1,4 +1,17 @@
-from cli import CLI
+import solvers
 
 if __name__ == "__main__":
-    cli = CLI()
+    print("Welcome to PDE-PLT, the linear partial differential equation plotter.\n")
+
+    highest_dt_order = int(input("Enter the highest order of a time derivative: "))
+
+    match highest_dt_order:
+        case 1:
+            solver = solvers.FirstOrderTime()
+
+        case _:
+            print("Not implemented")
+            solver = None
+
+    if solver:
+        solver.solve()
